ff3c2da6c7fad13554856b3d93eaa26cc49608ae
dumblefool <mergs257@gmail.com>
2016-09-27 07:45:00 +0530

    Update PrettyShort.py

--- PrettyShort.py
+++ PrettyShort.py
@@ -3,11 +3,13 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
-from nltk.tokenize.punkt import PunktSentenceTokenizer 
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 from sklearn.feature_extraction.text import TfidfTransformer,CountVectorizer
 import networkx
 import pyperclip
 import re
+from bs4 import BeautifulSoup
+import urllib2
 
 def Summarize(data):
     data=' '.join(data.strip().split('\n'))
@@ -20,12 +22,14 @@ def Summarize(data):
     rank=networkx.from_scipy_sparse_matrix(graph)
     scores=networkx.pagerank(rank)
     ordered=sorted(((scores[i],s) for i,s in enumerate(tokens)),reverse=True)
-    for j in range(0,3):
+    for j in range(0,2):
                 print ordered[j][1]
 
-input=str(pyperclip.paste())
-r1=re.sub(r'\bMr(?:\.|\b)',"Mr ",input)
-r2=re.sub(r'\bMrs(?:\.|\b)',"Mrs ",r1)
-r3=re.sub(r'\bDr(?:\.|\b)',"Dr ",r2)
-Summarize(r3)
-
+url=raw_input()
+page=urllib2.urlopen(url)
+soup=BeautifulSoup(page,"lxml")
+input=soup.find_all('p')
+data=u" "
+for p in input:
+    data=data+p.text
+Summarize(data)
